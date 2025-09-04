9a38b8ecfdee43b621b8f52b5b09539054ee362d
AngelZafirov <angel.zafirov@trendtec.de>
2025-09-04 09:20:24 +0200

    Update __manifest__.py

--- Odoo.addons/license_manager/__manifest__.py
+++ Odoo.addons/license_manager/__manifest__.py
@@ -3,8 +3,12 @@
     'version': '1.0',
     'summary': 'Verwaltung und API für Software-Lizenzen',
     'author': 'Dein Name',
+    'license': 'LGPL-3',   
     'depends': ['base', 'product'],
-    'data': [],
+    'data': [
+        'security/ir.model.access.csv',
+        'data/cron.xml',
+    ],
     'installable': True,
     'application': True,
 }
