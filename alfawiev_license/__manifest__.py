b11c2fa736f2eb41a249cc457749f3b51735f5ab
AngelZafirov <angel.zafirov@trendtec.de>
2025-09-17 08:42:12 +0200

    Create __manifest__.py

---
+++ alfawiev_license/__manifest__.py
@@ -0,0 +1,29 @@
+{
+    'name': 'Alfaview License',
+    'version': '1.0',
+    'summary': 'License management for software products',
+    'description': """
+        Manage software licenses linked to products and customers.
+        - Create license products
+        - Assign licenses to customers
+        - Track validity and expiration
+    """,
+    'author': 'TrendTec / Trendtec',
+    'website': 'https://www.trendtec.de',
+    'category': 'Sales',
+    'license': 'OPL-1',     # wichtig: zeigt es als Third-party App
+    'price': 100.0,         # Preis
+    'currency': 'EUR',      # Währung
+    'depends': ['base', 'contacts', 'sale'],
+    'data': [
+        'security/security.xml',
+        'security/ir.model.access.csv',
+        'views/product_views.xml',
+        'views/license_views.xml',
+        'views/customer_views.xml',
+        'wizards/license_wizard.xml',
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
